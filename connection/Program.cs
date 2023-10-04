83e9cd4afdfe5ef7d34808633c5ffdacf743adab
Fady Maher <80794740+fadymaher24@users.noreply.github.com>
2023-10-04 23:30:25 +0300

    upload files

---
+++ connection/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        // Define the IP address and port to listen on
+        IPAddress ipAddress = IPAddress.Parse("192.168.1.16");
+        int port = 8000;
+
+        // Create a TCP listener
+        TcpListener listener = new TcpListener(ipAddress, port);
+
+        try
+        {
+            // Start listening for incoming connctions
+            listener.Start();
+            Console.WriteLine($"Listening on {ipAddress}:{port}");
+
+            while (true)
+            {
+                // Accept incoming client connections
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine($"Accepted connection from {((IPEndPoint)client.Client.RemoteEndPoint).Address}:{((IPEndPoint)client.Client.RemoteEndPoint).Port}");
+
+                // Handle the client connection in a separate thread or method
+                // For example, you can create a new thread to handle each client:
+                // Thread clientThread = new Thread(() => HandleClient(client));
+                // clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            // Stop listening when done
+            listener.Stop();
+        }
+    }
+
+    // Define a method to handle client communication
+    // You can implement your specific logic here
+    static void HandleClient(TcpClient client)
+    {
+        // Implement your communication logic with the client here
+        // You can use the client.GetStream() method to read and write data
+    }
+}
---
+++ read_data/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("192.168.1.3");
+        int port = 8000;
+
+        TcpListener listener = new TcpListener(ipAddress, port);
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine($"Listening on {ipAddress}:{port}");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine($"Accepted connection from {((IPEndPoint)client.Client.RemoteEndPoint).Address}:{((IPEndPoint)client.Client.RemoteEndPoint).Port}");
+
+                _ = HandleClientAsync(client);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+
+    static async Task HandleClientAsync(TcpClient client)
+    {
+        try
+        {
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while (true)
+                {
+                    bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead <= 0)
+                        break;
+
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Received data: {receivedData}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
+
+
+
